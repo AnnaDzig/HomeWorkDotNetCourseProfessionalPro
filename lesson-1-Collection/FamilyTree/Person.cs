74a940086cb385cc59f72d52216534b68f74d4e3
AnnaDzig <adzyhota@protonmail.com>
2025-01-22 20:39:27 +0100

    add task Family Tree

---
+++ lesson-1-Collection/FamilyTree/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyTree
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public int YearOfBirth { get; set; }
+        public List<Person> Descendants
+        { get; set; } = new List<Person>();
+
+        public void AddDescendant(Person descendant)
+        { Descendants.Add(descendant); }
+        public void RemoveDescendant(string name) 
+        { Descendants.RemoveAll(d => d.Name == name); }
+        public IEnumerable< Person> GetDescendants()
+        {
+        return Descendants;
+        }
+        public IEnumerable<Person> GetDescendantsByYear(int year)
+            { return Descendants.Where(d => d.YearOfBirth == year); }
+        public override string ToString()
+        {
+                return $"{Name} {YearOfBirth}";
+            }
+        }
+    }
---
+++ lesson-1-Collection/FamilyTree/Program.cs
@@ -0,0 +1,43 @@
+﻿//Створіть колекцію, яка б за своєю структурою нагадувала «родове дерево»
+//(ім'я людини, рік народження), причому до неї можна додавати/вилучати нового родича,
+//є можливість побачити всіх спадкоємців обраної людини, відібрати родичів за роком народження.
+
+using FamilyTree;
+
+class Program
+{
+    static void Main()
+    {
+        // Створення кореня родового дерева
+        Person root = new Person { Name = "Anna", YearOfBirth = 1984 };
+
+        // Додавання спадкоємців
+        root.AddDescendant(new Person { Name = "Viktoriia", YearOfBirth = 2008 });
+        root.AddDescendant(new Person { Name = "Marcin", YearOfBirth = 1981 });
+        root.AddDescendant(new Person { Name = "Mihal", YearOfBirth = 2006 });
+        root.AddDescendant(new Person { Name = "Stepan", YearOfBirth = 1979 });
+
+        // Виведення всіх спадкоємців
+        Console.WriteLine("All descendants of Anna:");
+        foreach (var descendant in root.GetDescendants())
+        {
+            Console.WriteLine(descendant);
+        }
+
+        // Фільтрація за роком народження
+        Console.WriteLine("\nDescendants born in 1981:");
+        foreach (var descendant in root.GetDescendantsByYear(1981))
+        {
+            Console.WriteLine(descendant);
+        }
+
+        // Видалення спадкоємця
+        root.RemoveDescendant("Stepan");
+
+        Console.WriteLine("\nAfter removing Stepan:");
+        foreach (var descendant in root.GetDescendants())
+        {
+            Console.WriteLine(descendant);
+        }
+    }
+}
